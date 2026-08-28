5d4ed955e122f4751cf179f63a37a431462739de
ClutchMax <maxime.meunier45@gmail.com>
2026-08-28 18:26:02 +0200

    Added the mandebrot example

--- examples/mandelbrot/main.rs
+++ examples/mandelbrot/main.rs
@@ -1,22 +1,25 @@
-//! This is the source code of the first three subchapters from the "Using images" chapter at http://vulkano.rs.
-//!
-//! It is not commented, as the explanations can be found in the book itself.
-
 use std::sync::Arc;
 
 use image::{ImageBuffer, Rgba};
 use vulkano::buffer::{Buffer, BufferCreateInfo, BufferUsage};
 use vulkano::command_buffer::allocator::StandardCommandBufferAllocator;
 use vulkano::command_buffer::{
-    AutoCommandBufferBuilder, ClearColorImageInfo, CommandBufferUsage, CopyImageToBufferInfo,
+    AutoCommandBufferBuilder, CommandBufferUsage, CopyImageToBufferInfo,
 };
+use vulkano::descriptor_set::allocator::StandardDescriptorSetAllocator;
+use vulkano::descriptor_set::{DescriptorSet, WriteDescriptorSet};
 use vulkano::device::{Device, DeviceCreateInfo, QueueCreateInfo, QueueFlags};
-use vulkano::format::{ClearColorValue, Format};
+use vulkano::format::Format;
+use vulkano::image::view::ImageView;
 use vulkano::image::{Image, ImageCreateInfo, ImageType, ImageUsage};
 use vulkano::instance::{Instance, InstanceCreateFlags, InstanceCreateInfo};
 use vulkano::memory::allocator::{AllocationCreateInfo, MemoryTypeFilter, StandardMemoryAllocator};
-use vulkano::sync;
-use vulkano::sync::GpuFuture;
+use vulkano::pipeline::compute::ComputePipelineCreateInfo;
+use vulkano::pipeline::layout::PipelineDescriptorSetLayoutCreateInfo;
+use vulkano::pipeline::{
+    ComputePipeline, Pipeline, PipelineBindPoint, PipelineLayout, PipelineShaderStageCreateInfo,
+};
+use vulkano::sync::{self, GpuFuture};
 
 pub fn main() {
     let library = vulkano::VulkanLibrary::new().expect("no local Vulkan library/DLL");
@@ -56,9 +59,62 @@ pub fn main() {
 
     let queue = queues.next().unwrap();
 
+       
+    mod cs {
+            vulkano_shaders::shader! {
+            ty: "compute",
+            src: r"
+                #version 460
+
+                layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
+
+                layout(set = 0, binding = 0, rgba8) uniform writeonly image2D img;
+
+                void main() {
+                    vec2 norm_coordinates = (gl_GlobalInvocationID.xy + vec2(0.5)) / vec2(imageSize(img));
+
+                    vec2 c = (norm_coordinates - vec2(0.5)) * 2.0 - vec2(1.0, 0.0);
+
+                    vec2 z = vec2(0.0, 0.0);
+                    float i;
+                    for (i = 0.0; i < 1.0; i += 0.005) {
+                        z = vec2(
+                            z.x * z.x - z.y * z.y + c.x,
+                            z.y * z.x + z.x * z.y + c.y
+                        );
+
+                        if (length(z) > 20.0) {
+                            break;
+                        }
+                    }
+
+                    vec4 to_write = vec4(1 - i, 0.0, i, 1 - i);
+                    imageStore(img, ivec2(gl_GlobalInvocationID.xy), to_write);
+                }
+            ",
+        }
+    }
+
+    let shader = cs::load(device.clone()).expect("failed to craete shader module");
+
+    let cs = shader.entry_point("main").unwrap();
+    let stage = PipelineShaderStageCreateInfo::new(cs);
+    let layout = PipelineLayout::new(
+        device.clone(),
+        PipelineDescriptorSetLayoutCreateInfo::from_stages([&stage])
+            .into_pipeline_layout_create_info(device.clone())
+            .unwrap(),
+    )
+    .unwrap();
+
+    let compute_pipeline = ComputePipeline::new(
+        device.clone(),
+        None,
+        ComputePipelineCreateInfo::stage_layout(stage, layout),
+    )
+    .expect("Failed to create compute pipeline");
+
     let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
-    let command_buffer_allocator =
-        Arc::new(StandardCommandBufferAllocator::new(device.clone(), Default::default()));
 
     // Image creation
     let image = Image::new(
@@ -67,7 +123,7 @@ pub fn main() {
             image_type: ImageType::Dim2d,
             format: Format::R8G8B8A8_UNORM,
             extent: [1024, 1024, 1],
-            usage: ImageUsage::TRANSFER_DST | ImageUsage::TRANSFER_SRC,
+            usage: ImageUsage::STORAGE | ImageUsage::TRANSFER_SRC,
             ..Default::default()
         },
         AllocationCreateInfo {
@@ -76,6 +132,21 @@ pub fn main() {
         },
     )
     .unwrap();
+
+    let view = ImageView::new_default(image.clone()).unwrap();
+
+    let descriptor_set_allocator = 
+        Arc::new(StandardDescriptorSetAllocator::new(device.clone(), Default::default()));
+
+    let layout = compute_pipeline.layout().set_layouts().get(0).unwrap();
+    let set = DescriptorSet::new(
+        descriptor_set_allocator.clone(),
+        layout.clone(),
+        [WriteDescriptorSet::image_view(0, view)],
+        [],
+    )
+    .unwrap();
+
 
     let buf = Buffer::from_iter(
         memory_allocator.clone(),
@@ -92,20 +163,40 @@ pub fn main() {
     )
     .expect("failed to create buffer");
 
+    let command_buffer_allocator =
+        Arc::new(StandardCommandBufferAllocator::new(device.clone(), Default::default()));
+
+
     let mut builder = AutoCommandBufferBuilder::primary(
         command_buffer_allocator.clone(),
         queue.queue_family_index(),
         CommandBufferUsage::OneTimeSubmit,
     )
     .unwrap();
+
     builder
-        .clear_color_image(ClearColorImageInfo {
-            clear_value: ClearColorValue::Float([0.0, 0.0, 1.0, 1.0]),
-            ..ClearColorImageInfo::image(image.clone())
-        })
+        .bind_pipeline_compute(compute_pipeline.clone())
         .unwrap()
+        .bind_descriptor_sets(
+            PipelineBindPoint::Compute,
+            compute_pipeline.layout().clone(),
+            0,
+            set,
+        )
+        .unwrap();
+
+        // In this vulkano we need to put this unsafe block. It means WE need to make sure the shader
+        // is safe, as rustc can't.
+        unsafe {
+            builder.dispatch([1024 / 8, 1024 / 8, 1])
+                    .unwrap();
+        }
+        
+    builder
         .copy_image_to_buffer(CopyImageToBufferInfo::image_buffer(image, buf.clone()))
         .unwrap();
+
+
     let command_buffer = builder.build().unwrap();
 
     let future = sync::now(device)
@@ -119,7 +210,7 @@ pub fn main() {
     // Exporting the result
     let buffer_content = buf.read().unwrap();
     let image = ImageBuffer::<Rgba<u8>, _>::from_raw(1024, 1024, &buffer_content[..]).unwrap();
-    image.save("image.png").unwrap();
+    image.save("mandelbrot.png").unwrap();
 
     println!("Everything succeeded!");
 }
